ab7b757f418414fac0f6dd8a57d41346c02ce66e
cleave3 <owhiroroeghele@gmail.com>
2019-09-07 19:20:06 +0100

    Add page title

--- js/index.js
+++ js/index.js
@@ -1,18 +1,14 @@
 const result = document.getElementById('result');
 
 const getValue = (value) => {
-    if(result.value.length > 16) {
-        alert('Max value exceeded')
-    }
     result.value += value;
 }
 
 const calValue = () => {
     if (result.value == '') {
-      return  result.value = '';
-    } else {
-    result.value = eval(result.value)
+        return result.value == '' 
     }
+    result.value = eval(result.value)
 }
 
 const clearValue = (value) => {
